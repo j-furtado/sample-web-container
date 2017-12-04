1ac2f2e2e4d9dc25466e9726cbf3771e659e7f91
Joao Furtado <joao.furtado@hpe.com>
2017-12-04 11:48:34 +0000

    Added way to get the hostname;

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+  res.render('index', { 
+    locals: {
+      title: 'Express',
+      host: os.hostname()
+    }
+  });
 });
 
 module.exports = router;
